781e0fca64ed4e78a99fa545a603065a74314812
jinen abdelhak <jinenab123@gmail.com>
2019-11-21 01:27:32 +0100

    navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { TodoList, EditTodo, CreateTodo } from "./components";
+import logo from "./logo.png";
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <h2>MERN-Stack Todo App</h2>
-        <TodoList></TodoList>
-        <EditTodo></EditTodo>
-        <CreateTodo></CreateTodo>
-      </div>
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a
+              class="navbar-brand"
+              href="https://codingthesmartway.com"
+              target="_blank"
+            >
+              <img
+                src={logo}
+                width="30"
+                height="30"
+                alt="CodingTheSmartWay.com"
+              />
+            </a>
+            <Link to="/" className="navbar-brand">
+              MERN STACK Todo App
+            </Link>
+            <div className="collpase navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">
+                    Todos
+                  </Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">
+                    Create Todo
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <br></br>
+          <Route path="/" exact component={TodoList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
     );
   }
 }
